284b64fb8353b62edc8225943a102b662f71eeea
mac <Kunal123>
2019-04-08 18:51:55 -0400

    Modal Classes added.

---
+++ app/src/main/java/com/midtermmad3125/Modals/WeatherModal.java
@@ -0,0 +1,50 @@
+package com.midtermmad3125.Modals;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class WeatherModal {
+
+    private City city;
+    private Integer cnt;
+    private java.util.List<com.midtermmad3125.Modals.List> list = null;
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Integer getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(Integer cnt) {
+        this.cnt = cnt;
+    }
+
+    public java.util.List<com.midtermmad3125.Modals.List> getList() {
+        return list;
+    }
+
+    public void setList(java.util.List<com.midtermmad3125.Modals.List> list) {
+        this.list = list;
+    }
+
+}
